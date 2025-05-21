6281a1b18849efea1f99b5198121528ecb3672a8
Nicolas Canivares <nicocanivares2002@gmail.com>
2025-05-21 02:10:49 -0300

    Calculadora.cs creacion

---
+++ calculadora/Calculadora.cs
@@ -0,0 +1,27 @@
+namespace EspacioCalculadora
+{
+    class Calculadora
+    {
+        public void Sumar(double termino){
+            
+
+        }
+        public void Restar(double termino){
+
+
+        }
+
+        public void Multiplicar(double termino){
+
+
+        }
+        public void Dividir(double termino){
+
+
+        }
+        public void Limpiar(){
+
+
+        }
+    }
+}
