9695cf92fa18e97ca840622886b69f90fa8117ed
Guzman Canivares Enzo Nicolas <nicocanivares2002@gmail.com>
2025-06-02 22:27:21 -0300

    punto 1 tp7

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -2,3 +2,64 @@
 
 int numeroIngresado;
 bool aux = int.TryParse(Console.ReadLine(), out numeroIngresado);
+
+Calculadora calculadora1 = new Calculadora();
+
+int opcion = 0, bandera = 0; 
+double numero = 0;
+
+
+Console.WriteLine("---------- CALCULADORA ----------");
+
+do
+{
+    Console.WriteLine("Elige que operacion quieres realizar: \n 1)Sumar \n 2)Restar \n 3)Multiplicar \n 4)Dividir \n 5)Empezar un nuevo calculo \n 6)Salir de la calculadora");
+    int.TryParse(Console.ReadLine(), out opcion);
+
+    if (opcion != 5 && opcion != 6)
+    {
+        Console.WriteLine("Ingrese el numero para calcular:");
+        double.TryParse(Console.ReadLine(), out numero);
+    }
+    switch (opcion)
+    {
+        case 1:
+            calculadora1.Sumar(numero);
+            break;
+
+        case 2:
+            calculadora1.Restar(numero);
+            break;
+
+        case 3:
+            calculadora1.Multiplicar(numero);
+            break;
+
+        case 4:
+            calculadora1.Dividir(numero);
+            break;
+
+        case 5:
+            Console.WriteLine("Iniciando nuevo calculo...");
+            calculadora1.Limpiar();
+            break;
+
+        case 6:
+            Console.WriteLine("Saliendo de la calculadora...");
+            bandera = 1;
+            break;
+
+        default:
+            break;
+    }
+    if (opcion != 5 && opcion != 6)
+    {
+        Console.WriteLine("Tu resultado actual es:");
+        Console.WriteLine(calculadora1.Resultado.ToString());
+    }else
+    {
+        opcion = 0;
+    }
+
+
+} while (bandera != 1);
