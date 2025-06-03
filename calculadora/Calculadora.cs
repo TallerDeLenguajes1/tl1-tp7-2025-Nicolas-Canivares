8ff97e47b04de9d2e949f45f609b903052ef50ee
Guzman Canivares Enzo Nicolas <nicocanivares2002@gmail.com>
2025-06-02 22:23:11 -0300

    Calculadora.cs

--- calculadora/Calculadora.cs
+++ calculadora/Calculadora.cs
@@ -1,27 +1,37 @@
 namespace EspacioCalculadora
 {
-    class Calculadora
+    public class Calculadora
     {
-        public void Sumar(double termino){
-            
+        private double resultado=0;
 
+        public double Resultado { get => resultado; }
+
+        public void Sumar(double termino)
+        {
+            resultado += termino;
         }
-        public void Restar(double termino){
-
-
+        public void Restar(double termino)
+        {
+            resultado -= termino;
         }
 
-        public void Multiplicar(double termino){
-
-
+        public void Multiplicar(double termino)
+        {
+            resultado *= termino;
         }
-        public void Dividir(double termino){
-
-
+        public void Dividir(double termino)
+        {
+            if (termino != 0)
+            {
+                resultado /= termino;
+            }else
+            {
+                Console.WriteLine("No se puede dividir por 0");
+            }
         }
-        public void Limpiar(){
-
-
+        public void Limpiar()
+        {
+                    resultado = 0;
         }
     }
 }
